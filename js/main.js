a429172a9e0ac095a9372c984ec6743b8099d60f
spencer78610 <s_abbott150464@fanshaweonline.ca>
2023-04-10 05:47:30 -0400

    added java to the homepage

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
-console.log('js is working');
 
 let inputButton = document.querySelectorAll('#team button'),
     thebox = document.querySelector('#info-team'),
     theImg = document.querySelector('#info'),
     currentBoxButton = null;
+
+let ourBeer = document.querySelectorAll('#m-selling img'),
+    theBeer = document.querySelector('#beer-text'),
+    currentBeer = null;
+
 
 const buttonImages = {
   "spencer": "../images/matrix.png",
@@ -29,4 +33,20 @@ const buttonImages = {
         theImg.style.backgroundImage = "url(" + imgUrl + ")";
     }
 
+    function getbeerData() {
+      console.log('hey java');
+      if (currentBeer === this) {
+        theBeer.style.display = 'none';
+        currentBeer = null;
+      } else {
+        theBeer.style.display = "block";
+        theBeer.querySelector('.beer-title').textContent = beer[this.dataset.member].name;
+        theBeer.querySelector('.sell-text').textContent = beer[this.dataset.member].about;
+        currentBeer = this;
+      }
+    }
+    
+
     inputButton.forEach(button => button.addEventListener('click', getboxData));
+
+    ourBeer.forEach(beer => beer.addEventListener('click', getbeerData));
